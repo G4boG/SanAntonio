3d7e48b64b943a5a03563244c684e3e81f56cd2c
G4boG <gguaita66@gmail.com>
2026-06-23 12:58:54 -0400

    models terminado?

--- SanAntonio/models.py
+++ SanAntonio/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidato
-
+from django.core.validators import MinValueValidator
 class Escolar(models.Model):
     OPCIONES_CATEGORIA = [
         ('TEXTO', 'Libros/Diccionarios'),
@@ -8,3 +7,15 @@ class Escolar(models.Model):
         ('OFICINa', 'Plastificadoras/Resmas')
     ]
     
+    nombre = models.CharField(max_length=100)
+    categoria = models.CharField(
+        max_length=20,
+        choices=OPCIONES_CATEGORIA,
+        default='TEXTO'
+    )
+    stock = models.IntegerField(validators=[MinValueValidator(0,message="el stock no puede ser negativo")])
+    ubicacion = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f"{self.nombre} ({self.get_categoria_display()})"
